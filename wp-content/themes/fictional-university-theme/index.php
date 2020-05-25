82a63c6865292e53b2aa79efd437fbd7146d434f
Sarunas <sarunasindrulionis@gmail.com>
2020-05-25 12:27:52 +0300

    3.6. PHP funkcijos

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,10 @@
-Tai nauja tema
+<?php 
+function greet($name, $color){
+	echo "<p>Hi, my name is $name and my favorite color is $color.</p>";
+}
+greet('John', 'blue');
+greet('Jane', 'green');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
